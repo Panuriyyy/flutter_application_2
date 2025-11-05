b3e34caee23d92e072ac28aac8a7e252c84cc3bc
Panuriyyy <tanyatv22@gmail.com>
2025-11-05 16:28:58 +0300

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,64 +2,105 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              child: CircleAvatar(
-                radius: 50,
-                backgroundColor: Color.fromARGB(255, 188, 108, 166),
-                child: Text("Circle"),
-              ),
-            ),
-            Text("News", style: TextStyle(fontStyle: FontStyle.italic)),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    const MaterialApp(debugShowCheckedModeBanner: false, home: HomeRoute()),
+  );
+}
+
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
-                  decoration: BoxDecoration(
-                    color: Colors.lightGreen,
-                    shape: BoxShape.circle,
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Color.fromARGB(255, 188, 108, 166),
+                    child: Text("Circle"),
                   ),
-                  width: 50,
-                  height: 50,
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.lightGreen,
-                    shape: BoxShape.circle,
-                  ),
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.lightGreen,
-                    shape: BoxShape.circle,
-                  ),
-                  width: 50,
-                  height: 50,
+                Text("News", style: TextStyle(fontStyle: FontStyle.italic)),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        shape: BoxShape.circle,
+                      ),
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        shape: BoxShape.circle,
+                      ),
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        shape: BoxShape.circle,
+                      ),
+                      width: 50,
+                      height: 50,
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-
-        floatingActionButton: Center(
-          child: FloatingActionButton(
+          ),
+          ElevatedButton(
             onPressed: () {
-              // Действие при нажатии
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Cats()),
+              );
             },
-            child: Image.network(
-              "https://static.wikia.nocookie.net/3fca098f-258f-4af7-b8ec-470ddfa0a2ed",
-            ),
+            child: Text('Go to Cats'),
+          ),
+        ],
+      ),
+      floatingActionButton: Center(
+        child: FloatingActionButton(
+          onPressed: () {
+            // Действие при нажатии
+          },
+          child: Image.network(
+            "https://static.wikia.nocookie.net/3fca098f-258f-4af7-b8ec-470ddfa0a2ed",
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class Cats extends StatelessWidget {
+  const Cats({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Cats Page')),
+      body: Column(
+        children: [
+          Container(child: Text("This is cats page")),
+          Container(
+            child: Image.network(
+              "https://i.pinimg.com/736x/91/8b/a5/918ba5920e6067c5e0711c106d3cc544.jpg",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
